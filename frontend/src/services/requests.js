843a87e79ac4b74ee6b64dc8a9c6a2d7f2ad5a13
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-07-05 04:08:40 -0300

    refactor: axios refatored for testing

--- frontend/src/services/requests.js
+++ frontend/src/services/requests.js
@@ -1,25 +1,22 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:3001',
-  timeout: 1000,
-});
+const url = 'http://localhost:3001/task';
 
 export const requestGetAllTasks = async () => {
-  const { data } = await api.get('/task');
+  const { data } = await axios.get(url);
   return data;
 };
 
-export const requestDeleteTask = async (id) => api.delete('/task', { data: { id } });
+export const requestDeleteTask = async (id) => axios.delete(url, { data: { id } });
 
 export const requestPostTask = async ({ task, status, priority }) => {
-  await api.post('/task', { task, status, priority });
+  await axios.post(url, { task, status, priority });
 };
 
 export const requestPutTask = async ({
   id, task, status, priority,
 }) => {
-  await api.put('/task', {
+  await axios.put(url, {
     id, task, status, priority,
   });
 };
